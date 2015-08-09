091b5d888d95c3dc6d693a6150ee83e8091c28d1
nishisuke <nishisuke13+github@gmail.com>
2015-08-10 05:52:51 +0900

    Implement method

--- ET/app/models/toilet.rb
+++ ET/app/models/toilet.rb
@@ -3,22 +3,18 @@ class Toilet < ActiveRecord::Base
   has_many   :evaluations
 
   def self.mens(now_lat, now_long, dist = 0.02)
-    @toilets = Toilet.joins(:user)
-                     .where('user.sex = 0')
+    @toilets = Toilet.joins(:user).where(users: { sex: false })
                      .where('long < ? + ?', now_long, dist)
                      .where('long > ? - ?', now_long, dist)
                      .where('lat < ? + ?', now_lat, dist)
                      .where('lat > ? - ?', now_lat, dist)
-                     .preload(:user)
   end
 
-  def self.womens(now, dist = 0.02)
-    @toilets = Toilet.joins(:user)
-                     .where('user.sex = 1')
+  def self.womens(now_lat, now_long, dist = 0.02)
+    @toilets = Toilet.joins(:user).where(users: { sex: true })
                      .where('long < ? + ?', now_long, dist)
                      .where('long > ? - ?', now_long, dist)
                      .where('lat < ? + ?', now_lat, dist)
                      .where('lat > ? - ?', now_lat, dist)
-                     .preload(:user)
   end
 end
