2a263e9e2ee329dba460659a38bf7f300f6034d5
nishisuke <nishisuke13+github@gmail.com>
2015-08-10 05:04:51 +0900

    Change method

--- ET/app/models/toilet.rb
+++ ET/app/models/toilet.rb
@@ -2,23 +2,23 @@ class Toilet < ActiveRecord::Base
   belongs_to :user
   has_many   :evaluations
 
-  def self.mens(now, dist = 0.02)
+  def self.mens(now_lat, now_long, dist = 0.02)
     @toilets = Toilet.joins(:user)
                      .where('user.sex = 0')
-                     .where('long < ? + ?', now, dist)
-                     .where('long > ? - ?', now, dist)
-                     .where('lat < ? + ?', now, dist)
-                     .where('lat > ? - ?', now, dist)
+                     .where('long < ? + ?', now_long, dist)
+                     .where('long > ? - ?', now_long, dist)
+                     .where('lat < ? + ?', now_lat, dist)
+                     .where('lat > ? - ?', now_lat, dist)
                      .preload(:user)
   end
 
   def self.womens(now, dist = 0.02)
     @toilets = Toilet.joins(:user)
                      .where('user.sex = 1')
-                     .where('long < ? + ?', now, dist)
-                     .where('long > ? - ?', now, dist)
-                     .where('lat < ? + ?', now, dist)
-                     .where('lat > ? - ?', now, dist)
+                     .where('long < ? + ?', now_long, dist)
+                     .where('long > ? - ?', now_long, dist)
+                     .where('lat < ? + ?', now_lat, dist)
+                     .where('lat > ? - ?', now_lat, dist)
                      .preload(:user)
   end
 end
