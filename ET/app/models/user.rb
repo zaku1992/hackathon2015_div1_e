bee317694396ad5bb824eba1b5a84a5994261263
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-08-09 23:01:00 +0900

    ADD:column in user sex

--- ET/app/models/user.rb
+++ ET/app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true, uniqueness: true
+  validates :sex, inclusion:{in: [true, false]}
 end
--- ET/db/schema.rb
+++ ET/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809090957) do
+ActiveRecord::Schema.define(version: 20150809125522) do
 
   create_table "evaluations", force: :cascade do |t|
     t.integer  "user_id"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20150809090957) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.boolean  "sex"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
