6e10ee8bfda3a783882e2f687252c46a482ee0b6
nishisuke <nishisuke13+github@gmail.com>
2015-08-09 21:06:20 +0900

    routing

--- ET/config/routes.rb
+++ ET/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :evaluations
-  resources :toilets
   devise_for :users
   get 'home/index'
   root 'home#index'
+
+  resources :evaluations do
+    resources :toilets
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
